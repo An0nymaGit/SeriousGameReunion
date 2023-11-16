ba8276cbc8c1978ed187042ff35528d3e1390a90
PHILIPPON <m.philippon@rubika-edu.com>
2023-11-16 11:42:55 +0100

    Added DOTween to project

--- SeriousGameReunion/Assets/Scripts/GameManager.cs
+++ SeriousGameReunion/Assets/Scripts/GameManager.cs
@@ -1,26 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using DG.Tweening;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    public Transform chameleon;
 
+    [SerializeField] private float jumpHeight = 3;
+    [SerializeField] private float jumpPower = 5;
+    [SerializeField] private float jumpDuration = 2;
     
-    void Update()
+    private void Start()
     {
-        
+        chameleon.DOJump(chameleon.position + Vector3.up * jumpHeight, jumpPower, 1, jumpDuration);
     }
-}
-
-public enum Activity
-{
-    Cooking,
-    Exercise,
-    DoSomethingFun,
-    EatOrDrink,
-    Sleep,
 }
